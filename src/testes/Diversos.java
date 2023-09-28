724eecb69a2beee0ad76b732d3e22698310eeafc
Giovani <giovanifetzner01@gmail.com>
2023-09-28 17:25:35 -0300

    Atualização dos últimos testes usando gitHub integrado no eclipse

--- src/testes/Diversos.java
+++ src/testes/Diversos.java
@@ -1,5 +1,7 @@
 package testes;
 
+import java.time.LocalDate;
+
 public class Diversos {
 
 	public static void main(String[] args) {
@@ -8,9 +10,16 @@ public class Diversos {
 		int a = 5;
 		int b = 10;
 		String answer;
+		Integer c;
 		
 		answer =  (a>b)? "O valor de "+ a + " e maior que " + b : "O valor " + b +" e maior que " + a;
 		System.out.println(answer);
+		
+		// Data:
+		System.out.println("\n**Testes com Data**");
+		LocalDate data01 =  LocalDate.now();
+		System.out.println(data01);
+		//LocalDate.parse
 	}
 
 }
