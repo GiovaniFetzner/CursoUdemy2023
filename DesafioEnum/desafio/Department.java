c01071d335863672131f50184388a680650ac563
Giovani <giovanifetzner01@gmail.com>
2023-10-16 18:52:36 -0300

    Classes base criadas

--- DesafioEnum/desafio/Department.java
+++ DesafioEnum/desafio/Department.java
@@ -2,4 +2,19 @@ package desafio;
 
 public class Department {
 
+	private String name;
+
+	public Department(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
--- DesafioEnum/desafio/WorkerLevel.java
+++ DesafioEnum/desafio/WorkerLevel.java
@@ -1,5 +1,5 @@
 package desafio;
 
 public enum WorkerLevel {
-
+	JUNIOR, MID_LEVEL, SENIOR
 }
