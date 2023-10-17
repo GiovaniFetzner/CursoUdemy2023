b3084d8ff7a1ab5b2995476d6d7584b02972f93a
Giovani <giovanifetzner01@gmail.com>
2023-10-17 15:23:40 -0300

    Adição do ArrayList para controle dos contratos

--- DesafioEnum/desafio/Worker.java
+++ DesafioEnum/desafio/Worker.java
@@ -1,5 +1,54 @@
 package desafio;
 
+import java.util.ArrayList;
+
 public class Worker {
 
+	private String name;
+	private WorkerLevel level;
+	private double baseSalary;
+	
+	private Department department;
+	private ArrayList<HourContract> contracts = new ArrayList();
+	
+	public Worker(String name, WorkerLevel level, double baseSalary, Department department) {
+		this.name = name;
+		this.level = level;
+		this.baseSalary = baseSalary;
+		this.department = department;
+	}
+
+	public void addContract(HourContract contract) {
+		contracts.add(contract);
+	}
+	
+	public void removeContract(HourContract contract) {
+		contracts.remove(contract);
+	}
+	
+	public double income() {
+		
+		return 0;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public WorkerLevel getLevel() {
+		return level;
+	}
+	public void setLevel(WorkerLevel level) {
+		this.level = level;
+	}
+	public double getBaseSalary() {
+		return baseSalary;
+	}
+	public void setBaseSalary(double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+	
+	
 }
