8e516ef502d285aebc07944325e9d6314b122270
Giovani <giovanifetzner01@gmail.com>
2023-12-12 17:44:19 -0300

    atualização do git.ignore e da promoção do peão

--- projetoJogoXadrez/chess/ChessMatch.java
+++ projetoJogoXadrez/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -151,7 +150,7 @@ public class ChessMatch {
 			throw new IllegalStateException("Não há peça para ser promovida");
 		}
 		if(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-			throw new InvalidParameterException("Promoção inexistente");
+			return promoted;
 		}
 		
 		Position pos = promoted.getChessPosition().toPosition();
