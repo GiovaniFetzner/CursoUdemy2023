cef92d2ffc70e83d4061d5ceec7e907879b21f93
Giovani <giovanifetzner01@gmail.com>
2024-02-21 16:26:01 -0300

    Implementação do findAll()

--- projetoDao/application/Principal.java
+++ projetoDao/application/Principal.java
@@ -16,14 +16,25 @@ public class Principal {
 
 		System.out.println("Teste 1: findById() ");
 		Seller seller = sellerDao.findByID(3);
-
 		System.out.println(seller.toString());
 		System.out.println();
+		
 		System.out.println("Teste 2: findByDepartment() ");
-
 		List<Seller> list = sellerDao.findByDepartment(new Department(2, null));
 		list.forEach(System.out::println);
 		System.out.println();
+
+		System.out.println("Teste 3: findAll() ");
+		List<Seller> list01 = sellerDao.findAll();
+		list01.forEach(System.out::println);
+		
+		//Exercicio de ordenação usando stream e sorted
+		List<Seller> lista = list01.stream()
+				.sorted((x,y) -> x.getId().compareTo(y.getId()))
+				.toList();
+		System.out.println();
+		lista.forEach(System.out::println);
+
 
 	}
 
