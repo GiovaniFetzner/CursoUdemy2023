33a3be9c72b0fe442cc2a3656ae7e0efdff48012
Giovani <giovanifetzner01@gmail.com>
2024-02-21 11:41:25 -0300

    Implementação do findById e teste de verificação com o banco de dados

--- projetoDao/application/Principal.java
+++ projetoDao/application/Principal.java
@@ -2,17 +2,19 @@ package application;
 
 import java.util.Date;
 
+import entities.DaoFactory;
 import entities.Department;
 import entities.Seller;
+import entities.SellerDao;
 
 public class Principal {
 
 	public static void main(String[] args) {
 
-
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(21,"bob", "bob@gmail.com", new Date(), 3000.00, obj);
-		System.out.println(obj.toString());
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findByID(3);
+		
 		System.out.println(seller.toString());
 
 	}
---
+++ projetoDao/entities/DaoFactory.java
@@ -0,0 +1,11 @@
+package entities;
+
+import db.DB;
+
+public class DaoFactory {
+	
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC(DB.getConnection());
+	}
+
+}
