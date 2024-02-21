fe838706908ef320bdd9df7918a0049ae778e53c
Giovani <giovanifetzner01@gmail.com>
2024-02-21 10:40:44 -0300

    Criando classe Seller

--- projetoDao/application/Principal.java
+++ projetoDao/application/Principal.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import entities.Department;
+import entities.Seller;
 
 public class Principal {
 
@@ -8,7 +11,9 @@ public class Principal {
 
 
 		Department obj = new Department(1, "Books");
+		Seller seller = new Seller(21,"bob", "bob@gmail.com", new Date(), 3000.00, obj);
 		System.out.println(obj.toString());
+		System.out.println(seller.toString());
 
 	}
 
