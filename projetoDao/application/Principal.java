623780e7e6c741143c420f06cf64cb4b1d940911
Giovani <giovanifetzner01@gmail.com>
2024-02-21 18:47:15 -0300

    Implementação do delete() e update()

--- projetoDao/application/Principal.java
+++ projetoDao/application/Principal.java
@@ -7,6 +7,7 @@ import entities.DaoFactory;
 import entities.Department;
 import entities.Seller;
 import entities.SellerDao;
+import entities.SellerDaoJDBC;
 
 public class Principal {
 
@@ -18,7 +19,7 @@ public class Principal {
 		Seller seller = sellerDao.findByID(3);
 		System.out.println(seller.toString());
 		System.out.println();
-		
+
 		System.out.println("Teste 2: findByDepartment() ");
 		List<Seller> list = sellerDao.findByDepartment(new Department(2, null));
 		list.forEach(System.out::println);
@@ -27,14 +28,32 @@ public class Principal {
 		System.out.println("Teste 3: findAll() ");
 		List<Seller> list01 = sellerDao.findAll();
 		list01.forEach(System.out::println);
-		
-		//Exercicio de ordenação usando stream e sorted
-		List<Seller> lista = list01.stream()
-				.sorted((x,y) -> x.getId().compareTo(y.getId()))
-				.toList();
 		System.out.println();
-		lista.forEach(System.out::println);
 
+		// Exercicio de ordenação usando stream e sorted
+		System.out.println("Ordenacao dos vendedores pelo id:");
+		List<Seller> lista = list01.stream().sorted((x, y) -> x.getId().compareTo(y.getId())).toList();
+
+		lista.forEach(System.out::println);
+		System.out.println();
+		/*
+		 * System.out.println("Teste 4: seller insert() "); Seller newSeller = new
+		 * Seller(null, "Greg ", "greg@gmailcom", new Date(), 4000.00, (new
+		 * Department(3, null))); sellerDao.insert(newSeller);
+		 * System.out.println("Adicionado: " + newSeller.getId() + " departamento: " +
+		 * newSeller.getDepartment().getId());
+		 */
+
+		System.out.println("Teste 5: seller update() ");
+		seller = sellerDao.findByID(11);
+		seller.setName("Joao Witz");
+		seller.setEmail("joao@gmail.com");
+		sellerDao.update(seller);
+		System.out.println(sellerDao.findByID(11).toString());
+		System.out.println();
+
+		System.out.println("Teste 6: seller deleteById() ");
+		sellerDao.deleteById(10);
 
 	}
 
--- projetoDao/entities/SellerDaoJDBC.java
+++ projetoDao/entities/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -22,19 +23,101 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void insert(Seller seller) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+
+		try {
+			st = conn.prepareStatement(
+					"INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES "
+					+ "(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+
+			st.setString(1, seller.getName());
+			st.setString(2, seller.getEmail());
+			st.setDate(3, new java.sql.Date(seller.getBirthDate().getTime()));
+			st.setDouble(4, seller.getSalary());
+			st.setInt(5, seller.getDepartment().getId());
+
+			int rowsAffected = st.executeUpdate();
+
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					seller.setId(id);
+				}
+				DB.closeResultSet(rs);
+			} else {
+				throw new DbException("Error to insert");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DbException(e.getMessage());
+
+		} finally {
+			DB.closeStatement(st);
+
+		}
 
 	}
 
 	@Override
 	public void update(Seller seller) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+
+		try {
+			st = conn.prepareStatement(
+					"UPDATE seller "  
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? " 
+					+ "WHERE Id = ? ",
+					Statement.RETURN_GENERATED_KEYS);
+
+			st.setString(1, seller.getName());
+			st.setString(2, seller.getEmail());
+			st.setDate(3, new java.sql.Date(seller.getBirthDate().getTime()));
+			st.setDouble(4, seller.getSalary());
+			st.setInt(5, seller.getDepartment().getId());
+			st.setInt(6, seller.getId());
+			
+			st.executeUpdate();
+
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DbException(e.getMessage());
+
+		} finally {
+			DB.closeStatement(st);
+
+		}
 
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		
+		PreparedStatement st = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"DELETE FROM seller WHERE Id = ? ", 
+					Statement.RETURN_GENERATED_KEYS);
+			
+			st.setInt(1, id);
+			
+			st.executeUpdate();
+
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DbException(e.getMessage());
+
+		} finally {
+			DB.closeStatement(st);
+
+		}
 
 	}
 
@@ -94,10 +177,8 @@ public class SellerDaoJDBC implements SellerDao {
 
 		try {
 			st = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName " 
-					+ "FROM seller INNER JOIN department "
-					+ "ON seller.DepartmentId = department.Id " 
-					+ "ORDER BY Name ");
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "ORDER BY Name ");
 
 			rs = st.executeQuery();
 
@@ -105,15 +186,14 @@ public class SellerDaoJDBC implements SellerDao {
 			Map<Integer, Department> map = new HashMap<>();
 
 			while (rs.next()) {
-				
-				  //Código sugerido para evitar repetição do Department
-					Department dep = map.get(rs.getInt("DepartmentId"));
-				  
-					if (dep == null) {
-						dep = instantiateDepartment(rs);
-						map.put(rs.getInt("DepartmentId"), dep);
-					}
-				 
+
+				// Código sugerido para evitar repetição do Department
+				Department dep = map.get(rs.getInt("DepartmentId"));
+
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
 
 				Seller seller = instantiateSeller(rs, dep);
 				sellers.add(seller);
@@ -146,15 +226,13 @@ public class SellerDaoJDBC implements SellerDao {
 
 			while (rs.next()) {
 
-				
-				  //Código sugerido para evitar repetição do Department
-					Department dep = map.get(rs.getInt("DepartmentId"));
-				  
-					if (dep == null) {
-						dep = instantiateDepartment(rs);
-						map.put(rs.getInt("DepartmentId"), dep);
-					}
-				 
+				// Código sugerido para evitar repetição do Department
+				Department dep = map.get(rs.getInt("DepartmentId"));
+
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
 
 				Seller seller = instantiateSeller(rs, dep);
 				sellers.add(seller);
