3bf7360f3ccb4a9e26c0c95d2655136bde042d51
Giovani <giovanifetzner01@gmail.com>
2024-02-22 10:41:35 -0300

    Implementação do instantiateDepartment no DepartmentDaoJDBC

--- projetoDao/entities/DepartmentDaoJDBC.java
+++ projetoDao/entities/DepartmentDaoJDBC.java
@@ -6,12 +6,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
 import db.DbException;
 
-public class DepartmentDaoJDBC implements DepartmentDao{
-	
+public class DepartmentDaoJDBC implements DepartmentDao {
+
 	private Connection conn;
-	
+
 	public DepartmentDaoJDBC(Connection connection) {
 		this.conn = connection;
 	}
@@ -19,25 +20,53 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 	@Override
 	public void insert(Department department) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(Department department) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Department findByID(Integer id) {
-		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+
+			st = conn.prepareStatement("SELECT * FROM Department " + "WHERE Department.id = ?;");
+			st.setInt(1, id);
+
+			rs = st.executeQuery();
+
+			if (rs.next()) {
+				Department dep = instantiateDepartment(rs);
+			}
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+
 		return null;
+	}
+
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("Id"));
+		dep.setName(rs.getString("Name"));
+		return dep;
+
 	}
 
 	@Override
